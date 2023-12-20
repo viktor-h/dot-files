b4489e371d701d7c4ea9a6de3df2761ade1d53f1
Viktor Hartenberger <viktor.hartenberger@bookbeat.com>
2023-12-20 20:11:14 +0100

    cancel

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,3 +5,5 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 -- copy to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
