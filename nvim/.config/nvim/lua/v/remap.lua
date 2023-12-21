8855bfb2bec94f07c7c31d27ae0c9a1af0878724
Viktor Hartenberger <viktor.hartenberger@bookbeat.com>
2023-12-21 23:48:46 +0100

    nvim

--- nvim/.config/nvim/lua/v/remap.lua
+++ nvim/.config/nvim/lua/v/remap.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
--- nvim/.config/nvim/lua/v/set.lua
+++ nvim/.config/nvim/lua/v/set.lua
@@ -1,2 +1,3 @@
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.opt.scrolloff = 8
