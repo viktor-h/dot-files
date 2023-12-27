0bac86c70c7eec666f262724bab1619fcd8789ce
Viktor Hartenberger <viktor.hartenberger@bookbeat.com>
2023-12-27 11:12:22 +0100

    use after for plugin keymaps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,5 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      keys = {
-	{ "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Find Files" }
-      }
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
 }
