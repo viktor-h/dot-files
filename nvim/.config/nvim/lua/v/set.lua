aa65820399ec5bbcb58dbad7a8b74f0cd38a83a4
Viktor Hartenberger <viktor.hartenberger@bookbeat.com>
2023-12-27 16:33:17 +0100

    tabs to spaces

--- nvim/.config/nvim/lua/v/set.lua
+++ nvim/.config/nvim/lua/v/set.lua
@@ -1,3 +1,11 @@
 vim.opt.relativenumber = true
 vim.opt.number = true
+
 vim.opt.scrolloff = 8
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
