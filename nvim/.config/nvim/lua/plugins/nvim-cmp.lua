92653f184f4055c511cd6fe26939bfebffeb1929
Viktor Hartenberger <viktor.hartenberger@bookbeat.com>
2024-02-09 23:41:26 +0100

    autopairs

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -54,8 +54,6 @@ return {
                     end
                 end, { "i", "s" }),
 
-                ["<C-k>"] = cmp.mapping.select_prev_item(),
-                ["<C-j>"] = cmp.mapping.select_next_item(),
                 ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-e>"] = cmp.mapping.abort(),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }),
