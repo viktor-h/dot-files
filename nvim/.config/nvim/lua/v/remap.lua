a780d7c546f4b59d4f3622c854b1986e9b2234c3
Viktor Hartenberger <viktor.hartenberger@bookbeat.com>
2024-02-11 21:25:02 +0100

    move selected line up/down

--- nvim/.config/nvim/lua/v/remap.lua
+++ nvim/.config/nvim/lua/v/remap.lua
@@ -12,3 +12,6 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("v", "K", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '<-2<CR>gv=gv")
