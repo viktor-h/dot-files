1619f18223ad361c906b1be39a848daf23bfc315
Viktor Hartenberger <viktor.hartenberger@bookbeat.com>
2024-02-15 11:41:10 +0100

    ignore treesitter yaml

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,8 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "c_sharp", "javascript", "html", "markdown" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "c_sharp", "javascript", "html", "markdown" },
+            ignore_install = { "yaml" },
             sync_install = false,
             auto_install = true,
             highlight = { enable = true },
