e23db0658f51a33811cd1793c456e3e070aa2e59
Viktor Hartenberger <viktor.hartenberger@bookbeat.com>
2024-03-22 16:50:41 +0100

    Git status line

--- nvim/.config/nvim/lua/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,3 +1,6 @@
 return {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+    config = function ()
+        vim.cmd[[set statusline=%<%f\ %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%)\ %P]]
+    end
 }
