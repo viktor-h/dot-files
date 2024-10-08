3301394f23da09f1c771487ec3e6139fa0dc33e4
Viktor Hartenberger <viktordoth@gmail.com>
2024-10-08 12:23:00 +0200

    fix trouble

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,14 +1,5 @@
 return {
     "folke/trouble.nvim",
-    config = function()
-        require("trouble").setup {
-            icons = false,
-            signs = {
-                error = "e",
-                warning = "w",
-                hint = "h",
-                information = "i"
-            },
-        }
-    end
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble"
 }
