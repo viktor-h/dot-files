3116f27f6881d77b15268cbb7215dc097c1a0a92
Viktor Hartenberger <viktordoth@gmail.com>
2024-10-08 15:52:32 +0200

    Use LspAttach instead of on_attach. Also switch to maintained roslyn lsp fork

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -10,39 +10,37 @@ return {
 
         local keymap = vim.keymap
 
-        local on_attach = function(client, bufnr)
-            local opts = { buffer = bufnr, noremap = true, silent = true }
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+                -- Buffer local mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
 
-            -- disable semantic tokens from lsp
-            -- client.server_capabilities.semanticTokensProvider = nil
-            keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-            keymap.set('n', 'gr', "<cmd>Telescope lsp_references<CR>", opts)
-            keymap.set("n", " df", vim.diagnostic.goto_next, opts)
-            keymap.set("n", " dp", vim.diagnostic.goto_prev, opts)
-            keymap.set("n", " dl", "<cmd>Telescope diagnostics<CR>", opts)
-            keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, opts)
-            keymap.set("n", " r", vim.lsp.buf.rename, opts)
-            keymap.set('n', ' f', function()
-                vim.lsp.buf.format { async = true }
-            end, opts)
-            keymap.set({ 'n', 'v' }, ' ca', vim.lsp.buf.code_action, opts)
-        end
+                local opts = { buffer = ev.buf }
+
+                -- disable semantic tokens from lsp
+                -- client.server_capabilities.semanticTokensProvider = nil
+
+                keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                keymap.set('n', 'gr', "<cmd>Telescope lsp_references<CR>", opts)
+                keymap.set("n", " df", vim.diagnostic.goto_next, opts)
+                keymap.set("n", " dp", vim.diagnostic.goto_prev, opts)
+                keymap.set("n", " dl", "<cmd>Telescope diagnostics<CR>", opts)
+                keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, opts)
+                keymap.set("n", " r", vim.lsp.buf.rename, opts)
+                keymap.set('n', ' f', function()
+                    vim.lsp.buf.format { async = true }
+                end, opts)
+                keymap.set({ 'n', 'v' }, ' ca', vim.lsp.buf.code_action, opts)
+            end,
+        })
 
         -- autocompletion assign it to all language servers below
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
-        -- configure language servers
-        require("roslyn").setup({
-            dotnet_cmd = "dotnet",
-            roslyn_version = "4.8.0-3.23475.7",
-            on_attach = on_attach,
-            capabilities = capabilities
-        })
-
         lspconfig["lua_ls"].setup({
-            on_attach = on_attach,
             capabilities = capabilities,
             settings = {
                 Lua = {
@@ -54,7 +52,6 @@ return {
         })
 
         lspconfig["ts_ls"].setup({
-            on_attach = on_attach,
             capabilities = capabilities
         })
     end,
--- nvim/.config/nvim/lua/plugins/roslyn.lua
+++ nvim/.config/nvim/lua/plugins/roslyn.lua
@@ -1,3 +1,7 @@
 return {
-    "jmederosalvarado/roslyn.nvim"
+    "seblj/roslyn.nvim",
+    ft = "cs",
+    opts = {
+        -- your configuration comes here; leave empty for default settings
+    }
 }
