0025264e4780c4ac0f903c818fa175599b6dda70
viktor-h <viktordoth@gmail.com>
2025-11-27 00:01:44 +0100

    mason clean up

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,21 +1,9 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-        local mason = require("mason")
-
-        local mason_lspconfig = require("mason-lspconfig")
-
-        mason.setup()
-
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "lua_ls"
-            },
-
-            automatic_installation = true,
-        })
-    end,
+  "williamboman/mason.nvim",
+  opts = {
+    registries = {
+      "github:mason-org/mason-registry",
+      "github:Crashdummyy/mason-registry",
+    }
+  }
 }
