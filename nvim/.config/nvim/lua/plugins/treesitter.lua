818f2c7356ebaf50368bfe684f76744310e11ea1
viktor-h <viktordoth@gmail.com>
2025-12-02 14:14:33 +0100

    fix treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,13 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-
-        configs.setup({
-            ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "c_sharp", "javascript", "html", "markdown" },
-            ignore_install = { "yaml" },
-            sync_install = false,
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+    branch = 'main',
+    opts = {
+        ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "c_sharp", "javascript", "html", "markdown" },
+        auto_install = true,
+        highlight = {
+            enable = true,
+        },
+        indent = { enable = true }
+    }
 }
