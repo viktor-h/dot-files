bee98606fb0124b3c5273c952f8076d3628d7b4e
viktor-h <viktordoth@gmail.com>
2025-12-03 13:21:30 +0100

    use :TSInstall

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
     branch = 'main',
     opts = {
         ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "c_sharp", "javascript", "html", "markdown" },
-        auto_install = true,
         highlight = {
             enable = true,
         },
