36b072dcb65fa498f01f5c52dea8a22932adc008
viktor-h <viktordoth@gmail.com>
2026-02-01 01:05:32 +0100

    easy dotnet

---
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,23 @@
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+
+    vim.keymap.set("n", "q", function()
+      dap.terminate()
+      dap.clear_breakpoints()
+    end, { desc = "Terminate and clear breakpoints" })
+    vim.keymap.set("n", "<F5>", dap.continue, { desc = "Start/continue debugging" })
+    vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Step over" })
+    vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Step into" })
+    vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Step out" })
+    vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+    vim.keymap.set("n", "<leader>dO", dap.step_over, { desc = "Step over (alt)" })
+    vim.keymap.set("n", "<leader>dC", dap.run_to_cursor, { desc = "Run to cursor" })
+    vim.keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "Toggle DAP REPL" })
+    vim.keymap.set("n", "<leader>dj", dap.down, { desc = "Go down stack frame" })
+    vim.keymap.set("n", "<leader>dk", dap.up, { desc = "Go up stack frame" })
+
+    vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "", linehl = "DapBreakpoint", numhl = "" })
+  end
+}
---
+++ nvim/.config/nvim/lua/plugins/dotnet.lua
@@ -0,0 +1,17 @@
+return {
+  -- {
+  --   "seblyng/roslyn.nvim",
+  --   ---@module 'roslyn.config'
+  --   ---@type RoslynNvimConfig
+  --   opts = {
+  --     -- your configuration comes here; leave empty for default settings
+  --   },
+  -- },
+  {
+    "GustavEikaas/easy-dotnet.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "ibhagwan/fzf-lua", "mfussenegger/nvim-dap" },
+    config = function()
+      require("easy-dotnet").setup()
+    end
+  }
+}
