aae9b0831677a18c04e798f1f4b9981069621dcc
viktor-h <viktordoth@gmail.com>
2026-02-02 13:46:47 +0100

    remove after

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,4 +1,46 @@
 return {
   "ibhagwan/fzf-lua",
-  opts = {}
+  opts = {},
+  keys = {
+    {
+      "<leader>pf",
+      "<cmd>FzfLua files<CR>",
+      desc = "Find files",
+    },
+    {
+      "<C-p>",
+      "<cmd>FzfLua git_files<CR>",
+      desc = "Find git files",
+    },
+    {
+      "<leader>ps",
+      "<cmd>FzfLua grep<CR>",
+      desc = "Grep",
+    },
+    {
+      "<leader>vh",
+      "<cmd>FzfLua helptags<CR>",
+      desc = "Help tags",
+    },
+    {
+      "<leader>pws",
+      "<cmd>FzfLua grep_cword<CR>",
+      desc = "Grep word under cursor",
+    },
+    {
+      "<leader>pWs",
+      "<cmd>FzfLua grep_cWORD<CR>",
+      desc = "Grep WORD under cursor",
+    },
+    {
+      "<leader>bs",
+      "<cmd>FzfLua buffers<CR>",
+      desc = "Search buffers",
+    },
+    {
+      "<leader>vf",
+      "<cmd>FzfLua global<CR>",
+      desc = "Global search",
+    },
+  },
 }
